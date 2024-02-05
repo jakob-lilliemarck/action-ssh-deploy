acf942859fc9238d9609d934559b2de91a550cfa
Jakob Lilliemarck <lilliemarckjakob@gmail.com>
2024-02-05 20:53:30 +0100

    fix: passphrase

--- src/index.ts
+++ src/index.ts
@@ -10,35 +10,28 @@ const instructions = [
     { source: 'files/file2.txt', target: 'files/test2.txt' }
 ]
 
-const helper = async (config) => {
-    const queue = await UploadQueue.createUploadQueue(sshClient, config, instructions)
-    await queue.uploadAll()
+const main = async () => {
+    try {
+        const host = getInput("host");
+        const username = getInput("username")
+        const passphrase = getInput("password")
+        const privateKey = getInput("privateKey")
+
+        const config = {
+            host,           // '46.101.214.163',
+            port: 22,
+            username,       // 'root',
+            passphrase,     // 'test',
+            privateKey      // readFileSync('/home/jakob/.ssh/gh_id')
+        }
+
+        const queue = await UploadQueue.createUploadQueue(sshClient, config, instructions)
+        await queue.uploadAll()
+        sshClient.end()
+    } catch (e) {
+        // console.error('err', e)
+        setFailed(e.message);
+    }
 }
 
-try {
-    const host = getInput("host");
-    const username = getInput("username")
-    const passphrase = getInput("password")
-    const privateKey = getInput("privateKey")
-
-    const config = {
-        host,           // '46.101.214.163',
-        port: 22,
-        username,       // 'root',
-        passphrase,
-        privateKey      // readFileSync('/home/jakob/.ssh/id_rsa')
-    }
-
-    helper(config).then(() => {
-        console.info('Upload complete')
-        sshClient.end()
-    }).catch((e) => {
-        console.error('Upload error: ', e)
-    }).finally(() => {
-        sshClient.end()
-    })
-
-} catch (e) {
-    console.error('err', e)
-    setFailed(e.message);
-}   
+main()
